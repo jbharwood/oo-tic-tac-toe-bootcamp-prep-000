285f8f446f18d99a2f67b7cf142375742f312551
Joseph Harwood <josephblakeharwood@gmail.com>
2018-09-22 15:49:03 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,6 +34,12 @@ class TicTacToe
     board[index] = character
   end
   
-  
+  def position_taken?(board, index)
+    !(board[index].nil? || board[index] == " ")
+  end
+
+  def valid_move?(board, index)
+    index.between?(0,8) && !position_taken?(board, index)
+  end
   
 end
