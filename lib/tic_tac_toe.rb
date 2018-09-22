ad67c942666445f2e0aac32065ebd5ddac36d9d9
Joseph Harwood <josephblakeharwood@gmail.com>
2018-09-22 15:59:39 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -134,13 +134,13 @@ class TicTacToe
     end
   end
   
-  def play(board)
-    until over?(board) 
-      current_player(board)
-      turn(board)
+  def play()
+    until over?() 
+      current_player()
+      turn()
     end
-    if won?(board) 
-      puts "Congratulations #{winner(board)}!"
+    if won?() 
+      puts "Congratulations #{winner()}!"
     else
       puts "Cat's Game!"
     end
